6ea32354bd0c15949d7de5cd3728faf3b55986b9
zhouxudong <z_zxdong@163.com>
2016-09-22 09:21:28 +0800

    zhushi

--- webroot/index.js
+++ webroot/index.js
@@ -3,39 +3,27 @@ import { render }from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
 
 import Main from './common/Main'
-import AppManagerRoute from './routes/Appmanager'
-import APIManagerRoute from './routes/ApiManager'
-import {ProductManagerRoute, CategoryManagerRoute, StockManagerRoute } from './routes/ProductManager'
+//import AppManagerRoute from './routes/Appmanager'
+//import APIManagerRoute from './routes/ApiManager'
+//import {ProductManagerRoute, CategoryManagerRoute, StockManagerRoute } from './routes/ProductManager'
 import {NoticPushRoute, AdvertPutRoute, OrderManagerRoute, NewManagerRoute, MemberManagerRoute} from "./routes/RenRenGO"
-import ExamineRoute from './routes/Examine'
+//import ExamineRoute from './routes/Examine'
 const routes = {
     path: "/",
     component: Main,
     childRoutes: [
 
         //API后台管理
-        AppManagerRoute,
+        //AppManagerRoute,
 
         //ProductManager
-        ProductManagerRoute, CategoryManagerRoute, StockManagerRoute,
+        //ProductManagerRoute, CategoryManagerRoute, StockManagerRoute,
 
         //RenRenGO
         NoticPushRoute, AdvertPutRoute, OrderManagerRoute, NewManagerRoute, MemberManagerRoute,
 
         //资质审核
-        ExamineRoute
+        //ExamineRoute
     ]
 }
 render(<Router history={browserHistory} routes={routes}/>,document.getElementById("root"));
-
-//render((
-//        <Router history={browserHistory}>
-//            <Route path="/" component={Main}>
-//                <Route path="appmanager" route={AppManagerRoute}/>
-//                <Route path="apimanager" component={APIManager}/>
-//                <Route path="postman" component={POSTMan}/>
-//                <Route path="apidoc" component={APIDoc}/>
-//            </Route>
-//        </Router>
-//    ),document.getElementById("root")
-//)
