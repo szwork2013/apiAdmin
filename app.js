fe1e66aa28ee5f674346f609dab114717b8782ca
zhouxudong <z_zxdong@163.com>
2016-11-02 15:08:22 +0800

    port change

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use("/",(req,res,next) => {
   })
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(3200, function () {
   var host = server.address().address;
   var port = server.address().port;
 
